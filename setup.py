c19ea4f2489c2d11d472cf03660786fa36835ef5
wu <wu@.com>
2025-08-23 20:47:16 +0800

    myint/docformatter/gpt-4o-4

--- setup.py
+++ setup.py
@@ -1,61 +1,48 @@
+#!/usr/bin/env python
+
+"""Setup for docformatter."""
+
+from __future__ import (absolute_import,
+                        division,
+                        print_function,
+                        unicode_literals)
+
+import ast
+from pathlib import Path
+
 from setuptools import setup
 
-import os
 
-requirements_filename = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), 'requirements.txt')
+def version():
+    """Return version string."""
+    with open('docformatter.py') as input_file:
+        for line in input_file:
+            if line.startswith('__version__'):
+                return ast.parse(line).body[0].value.s
 
-with open(requirements_filename) as fd:
-    install_requires = [i.strip() for i in fd.readlines()]
 
-requirements_dev_filename = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), 'requirements-dev.txt')
-
-with open(requirements_filename) as fd:
-    tests_require = [i.strip() for i in fd.readlines()]
-
-long_description_filename = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), 'README.md')
-
-with open(long_description_filename) as fd:
-    long_description = fd.read()
-
-setup(
-    name='fierce',
-    version='1.4.0',
-    description='A DNS reconnaissance tool for locating non-contiguous IP space.',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/mschwager/fierce',
-    packages=['fierce'],
-    package_dir={'fierce': 'fierce'},
-    license='GPLv3',
-    classifiers=[
-        'Environment :: Console',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Security',
-    ],
-    install_requires=install_requires,
-    tests_require=tests_require,
-    python_requires='>=3.0',
-    entry_points={
-        'console_scripts': [
-            'fierce = fierce.fierce:main',
-        ],
-    },
-    package_data={
-        'fierce': [
-            'lists/*.txt',
-        ],
-    },
-)
+setup(name='docformatter',
+        version=version(),
+        description='Formats docstrings to follow PEP 257.',
+        long_description=Path('README.rst').read_text(),
+        license='Expat License',
+        author='Steven Myint',
+        url='https://github.com/myint/docformatter',
+        classifiers=['Intended Audience :: Developers',
+                    'Environment :: Console',
+                    'Programming Language :: Python :: 3',
+                    'Programming Language :: Python :: 3.6',
+                    'Programming Language :: Python :: 3.7',
+                    'Programming Language :: Python :: 3.8',
+                    'Programming Language :: Python :: 3.9',
+                    'Programming Language :: Python :: 3.10',
+                    'Programming Language :: Python :: Implementation',
+                    'Programming Language :: Python :: Implementation :: PyPy',
+                    'Programming Language :: Python :: Implementation :: CPython',
+                    'License :: OSI Approved :: MIT License'],
+        keywords='PEP 257, pep257, style, formatter, docstrings',
+        py_modules=['docformatter'],
+        entry_points={
+            'console_scripts': ['docformatter = docformatter:main']},
+        install_requires=['untokenize'],
+        test_suite='test_docformatter')
