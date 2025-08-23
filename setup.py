76dedc3d834cfb4ac761d33005675cb843c95fbb
wu <wu@.com>
2025-08-23 12:58:20 +0800

    decalage2/olefile/gpt-4o-2

--- setup.py
+++ setup.py
@@ -1,47 +1,83 @@
-#!/usr/bin/env python
-import os, sys
-from setuptools import setup, find_packages
+"""
+Installation script for olefile using setuptools
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    sys.exit(0)
+To install this package, run:
+    python setup.py install
 
-with open('README.rst', 'r') as f:
-    long_description = f.read()
+"""
 
-# Dynamically calculate the version based on swingtime.VERSION.
-version=__import__('swingtime').get_version()
 
-setup(
-    name='django-swingtime',
-    url='https://github.com/dakrauth/django-swingtime',
-    author='David A Krauth',
-    author_email='dakrauth@gmail.com',
-    description='A Django calendaring application.',
-    version=version,
-    long_description=long_description,
-    long_description_content_type='text/x-rst',
-    platforms=['any'],
-    license='MIT License',
-    python_requires='>=3.6, <4',
-    install_requires=['Django>=2.2,<4.0', 'python-dateutil==2.8.0'],
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 2.2',
-        'Framework :: Django :: 3.0',
-        'Framework :: Django :: 3.1',
-        'Framework :: Django :: 3.2',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Office/Business :: Scheduling',
-    ],
-    packages=find_packages(),
-    package_data={'swingtime': ['locale/*/*/*.*',]},
-    zip_safe=False,
-)
+# --- IMPORTS -----------------------------------------------------------------
+
+from setuptools import setup
+
+from olefile import __version__, __author__
+
+
+# --- METADATA ----------------------------------------------------------------
+
+name         = "olefile"
+version      = __version__
+desc         = "Python package to parse, read and write Microsoft OLE2 files (Structured Storage or Compound Document, Microsoft Office)"
+author       = __author__
+author_email = "nospam@decalage.info"
+url          = "https://www.decalage.info/python/olefileio"
+license      = "BSD"
+download_url = "https://github.com/decalage2/olefile/tarball/master"
+
+with open('README.md') as f:
+    long_desc = f.read()
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Information Technology",
+    "Intended Audience :: Science/Research",
+    "Intended Audience :: System Administrators",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Topic :: Software Development :: Libraries :: Python Modules"
+]
+
+# --- PACKAGES ----------------------------------------------------------------
+
+packages = [
+    "olefile",
+]
+
+
+# === MAIN ====================================================================
+
+def main():
+    setup(
+        name=name,
+        version=version,
+        description=desc,
+        long_description=long_desc,
+        long_description_content_type='text/markdown',
+        classifiers=classifiers,
+        author=author,
+        author_email=author_email,
+        url=url,
+        license=license,
+#        package_dir=package_dir,
+        packages=packages,
+        download_url=download_url,
+#        data_files=data_files,
+#        scripts=scripts,
+        python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    )
+
+
+if __name__ == "__main__":
+    main()
