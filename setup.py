d0e459f73a54af16119ff4bdcc28e07fcd65ecce
wu <wu@.com>
2025-08-24 11:52:48 +0800

    roskakori/pygount/gpt-4o-5

--- setup.py
+++ setup.py
@@ -1,47 +1,58 @@
+"""
+Setup for pygount.
+
+Developer cheat sheet
+---------------------
+
+Tag a release (simply replace ``1.x.x`` with the current version number)::
+
+  $ git tag -a -m "Tagged version 1.x.x" v1.x.x
+  $ git push --tags
+
+Upload release to PyPI::
+
+  $ python3 setup.py bdist_wheel
+  $ twine upload dist/*.whl
+"""
+# Copyright (c) 2016-2020, Thomas Aglassinger.
+# All rights reserved. Distributed under the BSD License.
+import os
 from setuptools import setup, find_packages
 
-version = "3.2.dev0"
+from pygount import __version__
 
-with open("README.rst", encoding="UTF-8") as file:
-    long_description = file.read() + "\n"
 
-with open("HISTORY.txt", encoding="UTF-8") as file:
-    long_description += file.read()
+# Read the long description from the README file.
+_setup_folder = os.path.dirname(__file__)
+with open(os.path.join(_setup_folder, "README.md"), encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
 
 setup(
-    name="pyroma",
-    version=version,
-    description="Test your project's packaging friendliness",
+    name="pygount",
+    version=__version__,
+    description="count source lines of code (SLOC) using pygments",
     long_description=long_description,
-    python_requires=">=3.6",
+    url="https://github.com/roskakori/pygount",
+    author="Thomas Aglassinger",
+    author_email="roskakori@users.sourceforge.net",
+    license="BSD",
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "License :: OSI Approved :: MIT License",
-        "Development Status :: 5 - Production/Stable",
+        "Topic :: Software Development",
     ],
-    keywords=["pypi", "quality", "testing"],
-    author="Lennart Regebro",
-    author_email="regebro@gmail.com",
-    url="https://github.com/regebro/pyroma",
-    project_urls={"Source Code": "https://github.com/regebro/pyroma"},
-    license="MIT",
-    packages=find_packages(exclude=["ez_setup"]),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        "setuptools",
-        "docutils",
-        "pygments",
-    ],
-    entry_points={
-        "console_scripts": ["pyroma = pyroma:main"],
-        "zest.releaser.prereleaser.before": ["pyroma = pyroma:zester"],
-    },
-    test_suite="pyroma",
+    keywords=["code analysis", "count", "SLOC"],
+    packages=find_packages(exclude=["tests"]),
+    install_requires=["pygments>=2.0"],
+    python_requires=">=3.5",
+    entry_points={"console_scripts": ["pygount=pygount.command:main"]},
 )
